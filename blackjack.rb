b0a4c78a34caef255c55d01c2e9ca5475bbf7ca7
Brady Swenson <bradyswenson@gmail.com>
2014-12-13 17:42:39 -0600

    blackjack progress

--- blackjack.rb
+++ blackjack.rb
@@ -23,6 +23,7 @@ class Deck
         @cards << Card.new(suit, face_value)
       end
     end
+    cards.shuffle!
   end
 
   def print
@@ -32,41 +33,100 @@ class Deck
     end
   end
 
-  def scramble!
-    cards.shuffle!
+  def deal_one
+    cards.shift
   end
 
-  def deal
-    cards.shift
+  def size
+    cards.size
   end
 end
 
+module Hand
+
+  def add_card(new_card)
+    cards << new_card
+  end
+
+  def draw_hand
+  line1 = ""
+  line2 = ""
+  line3 = ""
+  line4 = ""
+  cards.each do |card|
+    if card.value.length == 1
+      line1 << "┌───┐ "
+      line2 << "│ #{card.value} │ "
+      line3 << "│ #{card.suit} │ "
+      line4 << "└───┘ "
+    elsif card.value.length == 2
+      line1 << "┌───┐ "
+      line2 << "│ #{card.value}│ "
+      line3 << "│ #{card.suit} │ "
+      line4 << "└───┘ "
+    end     
+  end
+  puts line1
+  puts line2
+  puts line3
+  puts line4
+end
+
+end
+
 class Player
+  include Hand
+
+  attr_accessor :name, :cards
+
+  def initialize(name)
+    @name = name
+    @cards = []
+  end
 
 end
 
 class Dealer
+  include Hand
+
+  attr_accessor :name, :cards
+
+  def initialize
+    @name = "Dealer"
+    @cards = []
+  end
 
 end
+
+
 
 class Blackjack
   attr_accessor :player, :dealer, :deck
 
   def initialize
-    @player = Player.new
+    @player = Player.new("Brady")
     @dealer = Dealer.new
     @deck = Deck.new
 
   end
 
   def play
-    deck.scramble!
-    deck.print
-    # deal_cards
-    # show_flow
-    # player_turn
-    # dealer_turn
-    # who_won?
+    deck = Deck.new
+
+    player = Player.new("Chris")
+    player.add_card(deck.deal_one)
+    player.add_card(deck.deal_one)
+    player.add_card(deck.deal_one)
+    player.add_card(deck.deal_one)
+    puts player.cards
+    player.draw_hand
+
+    dealer = Dealer.new
+    dealer.add_card(deck.deal_one)
+    dealer.add_card(deck.deal_one)
+    dealer.add_card(deck.deal_one)
+    dealer.add_card(deck.deal_one)
+    dealer.draw_hand
   end
 
 end
